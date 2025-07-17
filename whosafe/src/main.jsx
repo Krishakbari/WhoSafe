f2d55a01f7d3e39f42d5dc16444639c47ce8d554
krish akbari <krishakbari82@gmail.com>
2025-07-17 12:37:25 +0530

    Fix static.json for SPA routing

--- whosafe/src/main.jsx
+++ whosafe/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from './context/auth.jsx'
 import { CartProvider } from './context/cartContex.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google"
